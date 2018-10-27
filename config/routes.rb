74c54ad561794fa0ba41b27a25d882507e36337b
NicGiles <n.r.giles1@gmail.com>
2018-10-27 17:34:52 +0100

    Added logout function to posts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
 
+  get 'posts/index'
   devise_for :users
 resources :users
-resources :posts
+resources :posts, :post
+
+
+authenticated :user do
+  root to: 'posts#index'
+end
 
   root to: redirect('/users/sign_in')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
