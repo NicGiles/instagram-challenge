bb8d312fffaf6d95047bbcd6e37d4e59a70f4786
NicGiles <n.r.giles1@gmail.com>
2018-10-27 16:07:02 +0100

    Initialized insta challenge with Rails and Devise

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+
+  devise_for :users
+resources :users
+resources :posts
+
+  root to: redirect('/users/sign_in')
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
